3db6688856255684893e7cd94b70402063d7057f
Mohamed Mamdouh <63814228+MohamedMamdouh18@users.noreply.github.com>
2021-02-01 17:14:51 +0200

    Create 11.sql

---
+++ Pset7/Movies/11.sql
@@ -0,0 +1,6 @@
+SELECT movies.title FROM movies
+join ratings on ratings.movie_id = movies.id  
+join stars on stars.movie_id = movies.id
+join people on people.id = stars.person_id
+where name = "Chadwick Boseman"
+order by rating desc limit 5 ;
